d930a99eee375ad214c4f14ea7d0e04368503676
NDALER <sdalvianto@protonmail.com>
2022-11-08 12:02:52 +0800

    patching center

--- config.h
+++ config.h
@@ -5,22 +5,18 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {"Firacode nerd font mono:size=10.5", "Noto Sans JP:size=10.5"};
+static const char *fonts[] = {
+	"monospace:size=10"
+};
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *dynamic     = NULL;      /* -dy option; dynamic command to run on input change */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#ffffff", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#ba157a" },
-	[SchemeOut] = { "#000000", "#ba157a" },
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeOut] = { "#000000", "#00ffff" },
 };
-/* -l and -g options; controls number of lines and columns in grid if > 0 */
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int columns    = 0;
-
-/* -h option; minimum height of a menu line */
-static unsigned int lineheight = 21;
-static unsigned int min_lineheight = 8;
 
 /*
  * Characters not considered part of a word while deleting words
